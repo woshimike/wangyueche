2cf1b2e71c5c5da831d28da653e6d4a1ac955e09
mike <466703629@qq.com>
2024-06-20 21:13:31 +0800

    start

--- src/main/java/com/example/ft2/test/Test.java
+++ src/main/java/com/example/ft2/test/Test.java
@@ -3,5 +3,8 @@ package com.example.ft2.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("===============搞起来====");
+        System.out.println("===============搞起来====");
+        System.out.println("===============搞起来====");
+        System.out.println("===============搞起来====");
     }
 }
