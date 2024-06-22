660cfe5848d80c29e725ba183c437a345111f53c
mike <466703629@qq.com>
2024-06-22 22:45:42 +0800

    start

--- src/main/java/com/example/ft2/test/Test.java
+++ src/main/java/com/example/ft2/test/Test.java
@@ -1,10 +1,16 @@
 package com.example.ft2.test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Test {
     public static void main(String[] args) {
+        //现在开始工作了 加油加油
         System.out.println("===============搞起来====");
         System.out.println("===============搞起来====");
         System.out.println("===============搞起来====");
         System.out.println("===============搞起来====");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(sdf.format(new Date()));
     }
 }
