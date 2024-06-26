277586f4be14456b3510e4a5aa4f5f04081944b5
mike <466703629@qq.com>
2024-06-26 21:09:20 +0800

    支付工具提交

---
+++ src/main/java/com/example/ft2/test/PayUtils.java
@@ -0,0 +1,401 @@
+package com.example.ft2.test;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.DigestUtils;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.io.*;
+import java.lang.reflect.Method;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.SignatureException;
+import java.util.*;
+
+
+/**
+ * 支付接口工具类
+ *
+ * @author lyq
+ * @Date 2018年4月17日 12:12:26
+ */
+public class PayUtils {
+
+    private static Logger logger = LoggerFactory.getLogger(PayUtils.class);
+    /**
+     * 拼接XML请求路径，获取签名
+     *
+     * @param m
+     * @return
+     */
+    public static String getRequestXML(Map<String, String> m) {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("<xml>");
+        Set<?> set = m.entrySet();
+        Iterator<?> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry entry = (Map.Entry) iterator.next();
+            String key = (String) entry.getKey();
+            String value = (String) entry.getValue();
+            //过滤相关字段sign
+//            if("sign".equalsIgnoreCase(key)){
+//                buffer.append("<"+key+">"+"<![CDATA["+value+"]]>"+"</"+key+">");
+//            }else{
+            buffer.append("<" + key + ">" + value + "</" + key + ">");
+//            }
+        }
+        buffer.append("</xml>");
+        return buffer.toString();
+    }
+
+    /**
+     * 生成32位随机数
+     *
+     * @return 随机数
+     */
+    public static String getRandomNumbers() {
+        Random random = new Random();
+        String result = "";
+        for (int i = 0; i < 32; i++) {
+            result += random.nextInt(10);
+        }
+        return result;
+    }
+
+    /**
+     * 签名字符串
+     *
+     * @param text          需要签名的字符串
+     * @param key           密钥
+     * @param input_charset 编码格式
+     * @return 签名结果
+     */
+    public static String sign(String text, String key, String input_charset) {
+        text = text + "&key=" + key;
+        return DigestUtils.md5DigestAsHex(getContentBytes(text, input_charset));
+    }
+
+    /**
+     * 签名字符串
+     *
+     * @param text          需要签名的字符串
+     * @param sign          签名结果
+     * @param key           密钥
+     * @param input_charset 编码格式
+     * @return 签名结果
+     */
+    public static boolean verify(String text, String sign, String key, String input_charset) {
+        text = text + key;
+        String mysign = DigestUtils.md5DigestAsHex(getContentBytes(text, input_charset)).toUpperCase();
+        if (mysign.equals(sign)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 签名字符串回调验签
+     *
+     * @param text          需要签名的字符串
+     * @param sign          签名结果
+     * @param key           密钥
+     * @param input_charset 编码格式
+     * @return 签名结果
+     */
+    public static boolean verifyNotify(String text, String sign, String key, String input_charset) {
+        text = text + "&key="+key;
+        String mysign = DigestUtils.md5DigestAsHex(getContentBytes(text, input_charset)).toUpperCase();
+        if (mysign.equals(sign)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 获取内容字节
+     *
+     * @param content
+     * @param charset
+     * @return
+     * @throws SignatureException
+     * @throws UnsupportedEncodingException
+     */
+    public static byte[] getContentBytes(String content, String charset) {
+        if (charset == null || "".equals(charset)) {
+            return content.getBytes();
+        }
+        try {
+            return content.getBytes(charset);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("MD5签名过程中出现错误,指定的编码集不对,您目前指定的编码集是:" + charset);
+        }
+    }
+
+
+    /**
+     * 除去数组中的空值和签名参数
+     *
+     * @param sArray 签名参数组
+     * @return 去掉空值与签名参数后的新签名参数组
+     */
+    public static Map<String, String> paraFilter(Map<String, String> sArray) {
+        Map<String, String> result = new HashMap<String, String>();
+        if (sArray == null || sArray.size() <= 0) {
+            return result;
+        }
+        for (String key : sArray.keySet()) {
+            String value = sArray.get(key);
+            if (value == null || value.equals("") || key.equalsIgnoreCase("sign")
+                    || key.equalsIgnoreCase("sign_type")) {
+                continue;
+            }
+            result.put(key, value);
+        }
+        return result;
+    }
+
+    /**
+     * 把数组所有元素排序，并按照“参数=参数值”的模式用“&”字符拼接成字符串
+     *
+     * @param params 需要排序并参与字符拼接的参数组
+     * @return 拼接后字符串
+     */
+    public static String createLinkString(Map<String, String> params) {
+        List<String> keys = new ArrayList<String>(params.keySet());
+        Collections.sort(keys);
+        String prestr = "";
+        for (int i = 0; i < keys.size(); i++) {
+            String key = keys.get(i);
+            String value = params.get(key);
+            if (i == keys.size() - 1) {// 拼接时，不包括最后一个&字符
+                prestr = prestr + key + "=" + value;
+            } else {
+                prestr = prestr + key + "=" + value + "&";
+            }
+        }
+        return prestr;
+    }
+
+    /**
+     * 把数组所有元素排序，并按照“参数=参数值”的模式用“&”字符拼接成字符串  去掉sign值
+     *
+     * @param params 需要排序并参与字符拼接的参数组
+     * @return 拼接后字符串
+     */
+    public static String createLinkStringRemoveSign(Map<String, String> params) {
+        List<String> keys = new ArrayList<String>(params.keySet());
+        Collections.sort(keys);
+        String prestr = "";
+        for (int i = 0; i < keys.size(); i++) {
+            String key = keys.get(i);
+            if (key.equals("sign")){
+                continue;
+            }
+            String value = params.get(key);
+            if (i == keys.size() - 1) {// 拼接时，不包括最后一个&字符
+                prestr = prestr + key + "=" + value;
+            } else {
+                prestr = prestr + key + "=" + value + "&";
+            }
+        }
+        return prestr;
+    }
+
+    /**
+     * 通过微信地址请求获取返回值
+     *
+     * @param requestUrl    请求地址
+     * @param requestMethod 请求方法
+     * @param outputStr     参数
+     */
+    public static String httpRequest(String requestUrl, String requestMethod, String outputStr) {
+        // 创建SSLContext
+        StringBuffer buffer = null;
+        try {
+            URL url = new URL(requestUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod(requestMethod);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.connect();
+            //往服务器端写内容
+            if (null != outputStr) {
+                OutputStream os = conn.getOutputStream();
+                os.write(outputStr.getBytes("utf-8"));
+                os.close();
+            }
+            // 读取服务器端返回的内容
+            InputStream is = conn.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is, "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            buffer = new StringBuffer();
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                buffer.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return buffer.toString();
+    }
+
+    /**
+     * 微信下载对账文件
+     *
+     * @param requestUrl    请求地址
+     * @param requestMethod 请求方法
+     * @param outputStr     参数
+     */
+    public static String httpRequestDownloadBill(String requestUrl, String requestMethod, String outputStr,
+                                                 String filePath, String fileName, String suffix) {
+        // 创建SSLContext
+        StringBuffer buffer = null;
+        try {
+            URL url = new URL(requestUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod(requestMethod);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.connect();
+            //往服务器端写内容
+            if (null != outputStr) {
+                OutputStream os = conn.getOutputStream();
+                os.write(outputStr.getBytes("utf-8"));
+                os.close();
+            }
+            // 读取服务器端返回的内容
+            InputStream is = conn.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is, "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            buffer = new StringBuffer();
+
+            String os = System.getProperty("os.name");
+            File file = null;
+            if(os.toLowerCase().startsWith("win")){
+                file = new File("d:\\"+filePath+"\\"+fileName+"."+suffix);
+            } else {
+                file = new File("/opt/teaB/"+filePath+"/"+fileName+"."+suffix);
+            }
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                bw.write(line);
+            }
+            bw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return buffer.toString();
+    }
+
+    public static String urlEncodeUTF8(String source) {
+        String result = source;
+        try {
+            result = java.net.URLEncoder.encode(source, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            logger.error("PayUtils httpRequest()服务器内容读取失败", e);
+        }
+        return result;
+    }
+
+    /**
+     * 解析xml,返回第一级元素键值对。如果第一级元素有子节点，则此节点的值是子节点的xml数据。
+     *
+     * @param strxml
+     * @return
+     * @throws IOException
+     */
+    public static Map doXMLParse(String strxml) throws Exception {
+        if (null == strxml || "".equals(strxml)) {
+            return null;
+        }
+        Map m = new HashMap();
+        InputStream in = String2Inputstream(strxml);
+        SAXBuilder builder = new SAXBuilder();
+        Document doc = builder.build(in);
+        Element root = doc.getRootElement();
+        List list = root.getChildren();
+        Iterator it = list.iterator();
+        while (it.hasNext()) {
+            Element e = (Element) it.next();
+            String k = e.getName();
+            String v = "";
+            List children = e.getChildren();
+            if (children.isEmpty()) {
+                v = e.getTextNormalize();
+            } else {
+                v = getChildrenText(children);
+            }
+            m.put(k, v);
+        }
+        //关闭流
+        in.close();
+        return m;
+    }
+
+    /**
+     * 获取子结点的xml
+     *
+     * @param children
+     * @return String
+     */
+    public static String getChildrenText(List children) {
+        StringBuffer sb = new StringBuffer();
+        if (!children.isEmpty()) {
+            Iterator it = children.iterator();
+            while (it.hasNext()) {
+                Element e = (Element) it.next();
+                String name = e.getName();
+                String value = e.getTextNormalize();
+                List list = e.getChildren();
+                sb.append("<" + name + ">");
+                if (!list.isEmpty()) {
+                    sb.append(getChildrenText(list));
+                }
+                sb.append(value);
+                sb.append("</" + name + ">");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static InputStream String2Inputstream(String str) {
+        return new ByteArrayInputStream(str.getBytes());
+    }
+
+    public static Map<String, String> objectToMap(Object obj) throws Exception {
+        if(obj == null)
+            return null;
+
+        Map<String, String> map = new HashMap<String, String>();
+
+        BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+        for (PropertyDescriptor property : propertyDescriptors) {
+            String key = property.getName();
+            if (key.compareToIgnoreCase("class") == 0) {
+                continue;
+            }
+            Method getter = property.getReadMethod();
+            Object value = getter!=null ? getter.invoke(obj) : null;
+            map.put(key, value.toString());
+        }
+
+        return map;
+    }
+
+}
