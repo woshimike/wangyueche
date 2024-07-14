c4f4e00395630b56d214c6ec1fee4502ab248272
mike <466703629@qq.com>
2024-07-14 17:56:39 +0800

    测试类

---
+++ src/test/java/com/example/ft2/t2/T2ServiceImplTest.java
@@ -0,0 +1,15 @@
+package com.example.ft2.t2;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class T2ServiceImplTest {
+    @Test
+    public void name1() {
+    }
+
+    @Test
+    public void name() {
+        
+    }
+}
