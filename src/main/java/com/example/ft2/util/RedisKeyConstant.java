06b9416bdf965f91d365eccfbdb357ca2278f4fe
mike <466703629@qq.com>
2024-08-02 22:03:39 +0800

    水电费留点时间

--- src/main/java/com/example/ft2/util/RedisKeyConstant.java
+++ src/main/java/com/example/ft2/util/RedisKeyConstant.java
@@ -17,7 +17,7 @@ public class RedisKeyConstant {
     //房间里的人员流ID set
     public static String roomStreamIdKey = "mix_room_stream_id_";
     //同一个用户在同一个房间内 3小时内只有一个 视频录制任务
-    public static String createVideoKey = "create_video_";
+    public static String createVideoKey = "create_video_123";
     //用户当天视频通话消耗时间
-    public static String userVideoConsume = "user_video_consume_";
+    public static String userVideoConsume = "user_video_consume_lyq";
 }
