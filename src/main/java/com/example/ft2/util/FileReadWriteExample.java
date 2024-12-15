abe8860e9d429fc5b73376d22df49492889e6388
mike <466703629@qq.com>
2024-12-15 18:20:54 +0800

    新增参数

--- src/main/java/com/example/ft2/util/FileReadWriteExample.java
+++ src/main/java/com/example/ft2/util/FileReadWriteExample.java
@@ -10,6 +10,9 @@ public class FileReadWriteExample {
     public static void main(String[] args) {
         String inputFileName = "input123231231123.txt";
         String outputFileName = "outpu123123t.txt";
+        String outputFileName2 = "outpu123123t.txt";
+        String outputFileName3 = "outpu123123t.txt";
+
         /**
          *
          * sdfjlsdjf加点住是吧
